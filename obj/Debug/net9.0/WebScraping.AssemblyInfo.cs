cd7ace2d33991462d4dd94dc80d4b572d26a56a9
judejay <judejay26@gmail.com>
2025-04-16 18:34:26 +0100

    Update AssemblyInformationalVersion and cache file for WebScraping project

--- obj/Debug/net9.0/WebScraping.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebScraping.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebScraping")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+165d3680b906127c812e08cf499e72f31c622845")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebScraping")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebScraping")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
